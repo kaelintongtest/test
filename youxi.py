7d6d80969808d19072d735be20ba562a7d7dc8dd
Kaelin <693379154@qq.com>
2018-03-20 16:39:41 +0800

    测试

---
+++ youxi.py
@@ -0,0 +1,23 @@
+'''
+Heroes beta-0.1
+Kaelin
+2017-09-01
+'''
+hp = 100
+print('welcome Heroes world!')
+print("| the world is like this ##### , 'a' for left, 'd' for right |")
+name = input('input you name:')
+
+if not name:
+	name = 'player1'
+usermsg = [name,hp]
+
+print("your hero's name is :",usermsg[0], 'hp is :' , usermsg[1])
+print("and now you are here : *####' | 'a' for left, 'd' for right |") 
+
+
+userinput = input()
+if userinput == 'd':
+	print("your are here #*###")
+if userinput == 'a':
+    print("your are here *####")
